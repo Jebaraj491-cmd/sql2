d4903c77ad88a0d54c622435c3629890b2425472
Jebaraj491-cmd <75720617+Jebaraj491-cmd@users.noreply.github.com>
2023-04-16 11:00:40 +0530

    Add files via upload

---
+++ sqlsample.sql
@@ -0,0 +1,8 @@
+Create database players;
+
+create table cricketers(player_id int,name varchar(50))
+
+insert into cricketers (player_id, name) values (1,'Sachin');
+insert into cricketers (player_id, name) values (2,'Dhoni');
+
+Select * from cricketers;
